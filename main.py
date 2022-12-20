1399bcd32797f3b348a1b97ff3c9b87a681b0556
addie-tyc <addiechung.tyc@gmail.com>
2022-12-20 17:26:40 +0800

    feat: register posts crawler

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from crawlers.TiktokAccountCrawler import TiktokAccountCrawler
+from crawlers.TiktokPostsCrawler import TiktokPostsCrawler
 
 if __name__ == '__main__':
     links = TiktokAccountCrawler('@archieandaxing').run()
+    TiktokPostsCrawler(links).run()
