6a2a852d7b382bd003e00889502503631bc97e0b
addie-tyc <addiechung.tyc@gmail.com>
2022-12-20 21:51:00 +0800

    fix: install driver and browser before running

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+import os
+
+from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 from crawlers.TiktokAccountCrawler import TiktokAccountCrawler
 from crawlers.TiktokPostsCrawler import TiktokPostsCrawler
 
-options = Options() 
-options.add_argument('--no-sandbox') # for error: DevToolsActivePort file doesn't exist
-options.add_argument('--headless')  
-options.add_argument('--disable-gpu')
+load_dotenv()
+
+options = Options()
+# for error: DevToolsActivePort file doesn't exist
+options.add_argument('start-maximized') 
+options.add_argument('disable-infobars')
+options.add_argument('--disable-extensions')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--no-sandbox')
+options.add_argument('--headless')
+options.binary_location = os.getenv('BINARY_LOC') # need to install chromium first
 
 if __name__ == '__main__':
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver = webdriver.Chrome(options=options)
     links = TiktokAccountCrawler('@archieandaxing', driver).run()
     TiktokPostsCrawler(links, driver).run()
     driver.close()
